d6847fe895f4c8e1c52b09c5832191e976c76f4f
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2024-04-17 15:12:25 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
+// import './App.css'
+// const url = 'https://staging--adminbpr.netlify.app'
 const url = 'http://localhost:3000'
 
 function App() {
   const iRef = useRef<HTMLIFrameElement | null>(null);
+
+  useEffect(() => {
+    const messageHandler = (event: MessageEvent) => {
+      console.log(event.data, event.origin)
+    };
+
+    window.addEventListener('message', messageHandler);
+
+    return () => {
+      window.removeEventListener('message', messageHandler);
+    };
+  }, []);
 
   const handleLoad = () => {
     if (iRef?.current?.contentWindow) {
@@ -26,11 +40,21 @@ function App() {
     }
   }
 
+  const checkIsLoggedIn = () => {
+    if (iRef?.current?.contentWindow) {
+      iRef.current.contentWindow.postMessage({
+        eventMessage: 'checkIsLoggedIn'
+      }, '*');
+    }
+  }
+
   return (
     <div className="card">
+      {url}
       <iframe src={url} width={1000} height={1000} ref={iRef} onLoad={handleLoad} />
       <button onClick={hideNavbar}>Hide Navbar</button>
       <button onClick={logout}>Logout</button>
+      <button onClick={checkIsLoggedIn}>checkIsLoggedIn</button>
     </div>
   )
 }
